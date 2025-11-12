71c48f4315655602f6eb4bad2e37f0fd6a82b29d
Alexis2ds <alexisvargasdiaz81@gmail.com>
2025-11-12 15:29:27 -0600

    feat: Mainclass

---
+++ Mainclass.java
@@ -0,0 +1,12 @@
+// Sistema de Gestión - Clientes 
+// Proyecto desarrollado por [Alexis]
+// Fecha: Noviembre 2025
+import controlador.MDIprincipal;
+
+
+public class Mainclass {
+    // Método principal - ejecuta la interfaz MDI
+    public static void main(String[] args) {
+        new MDIprincipal().setVisible(true);
+    }
+}
